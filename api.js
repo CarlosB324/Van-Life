11332fdbcf2f89a90a8f7d11829b9111da04a54f
CarlosB324 <127149062+CarlosB324@users.noreply.github.com>
2024-10-22 17:17:03 -0400

    test api

--- api.js
+++ api.js
@@ -1,101 +1,72 @@
-import { initializeApp } from "firebase/app"
-import {
-    getFirestore,
-    collection,
-    doc,
-    getDocs,
-    getDoc,
-    query,
-    where,
-    documentId
-} from "firebase/firestore/lite"
+// import { initializeApp } from "firebase/app"
+// import {
+//     getFirestore,
+//     collection,
+//     doc,
+//     getDocs,
+//     getDoc,
+//     query,
+//     where,
+//     documentId
+// } from "firebase/firestore/lite"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAxcsuUCggPJv52Fg2EoueAAoR8mmE9nFw",
-    authDomain: "vanlife-88fe3.firebaseapp.com",
-    projectId: "vanlife-88fe3",
-    storageBucket: "vanlife-88fe3.appspot.com",
-    messagingSenderId: "497944607409",
-    appId: "1:497944607409:web:76d08640f0c35c6c718090"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAxcsuUCggPJv52Fg2EoueAAoR8mmE9nFw",
+//     authDomain: "vanlife-88fe3.firebaseapp.com",
+//     projectId: "vanlife-88fe3",
+//     storageBucket: "vanlife-88fe3.appspot.com",
+//     messagingSenderId: "497944607409",
+//     appId: "1:497944607409:web:76d08640f0c35c6c718090"
+//   };
 
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app)
+// const app = initializeApp(firebaseConfig)
+// const db = getFirestore(app)
 
-// Refactoring the fetching functions below
-const vansCollectionRef = collection(db, "vans")
+// const vansCollectionRef = collection(db, "vans")
 
-export async function getVans() {
-    const snapshot = await getDocs(vansCollectionRef)
-    const vans = snapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-    }))
-    return vans
-}
+// export async function getVans() {
+//     const snapshot = await getDocs(vansCollectionRef)
+//     const vans = snapshot.docs.map(doc => ({
+//         ...doc.data(),
+//         id: doc.id
+//     }))
+//     return vans
+// }
 
-export async function getVan(id) {
-    const docRef = doc(db, "vans", id)
-    const snapshot = await getDoc(docRef)
-    return {
-        ...snapshot.data(),
-        id: snapshot.id
-    }
-}
+// export async function getVan(id) {
+//     const docRef = doc(db, "vans", id)
+//     const snapshot = await getDoc(docRef)
+//     return {
+//         ...snapshot.data(),
+//         id: snapshot.id
+//     }
+// }
 
-export async function getHostVans() {
-    const q = query(vansCollectionRef, where("hostId", "==", "123"))
-    const snapshot = await getDocs(q)
-    const vans = snapshot.docs.map(doc => ({
-        ...doc.data(),
-        id: doc.id
-    }))
-    return vans
-}
-
-/* 
-This 👇 isn't normally something you'd need to do. Instead, you'd 
-set up Firebase security rules so only the currently logged-in user 
-could edit their vans.
-
-https://firebase.google.com/docs/rules
-
-I'm just leaving this here for educational purposes, as it took
-me a while to find the `documentId()` function that allows you
-to use a where() filter on a document's ID property. (Since normally
-it only looks at the data() properties of the document, meaning you
-can't do `where("id", "==", id))`
-
-It also shows how you can chain together multiple `where` filter calls
-*/
-
-// export async function getHostVan(id) {
-//     const q = query(
-//         vansCollectionRef,
-//         where(documentId(), "==", id),
-//         where("hostId", "==", "123")
-//     )
+// export async function getHostVans() {
+//     const q = query(vansCollectionRef, where("hostId", "==", "123"))
 //     const snapshot = await getDocs(q)
 //     const vans = snapshot.docs.map(doc => ({
 //         ...doc.data(),
 //         id: doc.id
 //     }))
-//     return vans[0]
+//     return vans
 // }
 
-export async function loginUser(creds) {
-    const res = await fetch("/api/login",
-        { method: "post", body: JSON.stringify(creds) }
-    )
-    const data = await res.json()
 
-    if (!res.ok) {
-        throw {
-            message: data.message,
-            statusText: res.statusText,
-            status: res.status
-        }
-    }
 
-    return data
-}
+// export async function loginUser(creds) {
+//     const res = await fetch("/api/login",
+//         { method: "post", body: JSON.stringify(creds) }
+//     )
+//     const data = await res.json()
+
+//     if (!res.ok) {
+//         throw {
+//             message: data.message,
+//             statusText: res.statusText,
+//             status: res.status
+//         }
+//     }
+
+//     return data
+// }
